f2809c2067fbb6fd69fefc331547135f7ff06538
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-10-28 09:34:05 +0100

    ch10.Ex14

--- src/main/scala/pl/softech/learning/ch10/Ex13.scala
+++ src/main/scala/pl/softech/learning/ch10/Ex13.scala
@@ -3,8 +3,6 @@ package pl.softech.learning.ch10
 import pl.softech.learning.ch10.FoldableInstances._
 import pl.softech.learning.ch3.{Branch, Leaf, Tree}
 
-import scala.annotation.tailrec
-
 object Ex13 {
 
   trait FoldableInstances {
@@ -15,14 +13,19 @@ object Ex13 {
         case Leaf(a) => f(a, z)
         case Branch(left, Leaf(a)) => f(a, foldRight(left)(z)(f))
         case Branch(Leaf(a), right) => f(a, foldRight(right)(z)(f))
+        case Branch(left, right) => {
+          val acc = foldRight(right)(z)(f)
+          foldRight(left)(acc)(f)
+        }
       }
 
       override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B = {
-        @tailrec
+
         def go(t: Tree[A], acc: B): B = t match {
           case Leaf(a) => f(acc, a)
           case Branch(left, Leaf(a)) => go(left, f(acc, a))
           case Branch(Leaf(a), right) => go(right, f(acc, a))
+          case Branch(left, right) => go(left, go(right, acc))
         }
 
         go(as, z)
@@ -34,7 +37,7 @@ object Ex13 {
 
   def main(args: Array[String]): Unit = {
 
-    val tree = Branch(Leaf(1), Leaf(2))
+    val tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
 
     def suml(t: Tree[Int]): Int = Foldable[Tree].foldLeft(t)(0)(_ + _)
 
--- src/main/scala/pl/softech/learning/ch10/Ex15.scala
+++ src/main/scala/pl/softech/learning/ch10/Ex15.scala
@@ -1,8 +1,25 @@
 package pl.softech.learning.ch10
 
+import pl.softech.learning.ch10.FoldableInstances._
+import pl.softech.learning.ch3.{Branch, Leaf, Tree}
+
 object Ex15 {
 
+  def toList[A, F[_] : Foldable](fa: F[A]): List[A] = {
+    Foldable[F].foldLeft(fa)(List.empty[A]) { (acc, a) =>
+      a :: acc
+    }
+  }
+
   def main(args: Array[String]): Unit = {
+
+    val tree: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+
+    println(toList(tree))
+
+    val seq = IndexedSeq(1, 2, 3, 4, 5, 6)
+    
+    println(toList(seq))
 
   }
 
