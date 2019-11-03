455566a09fd1aabb3cdea3c90429c455142edf21
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-11-03 18:16:04 +0100

    ch11.Ex5

--- src/main/scala/pl/softech/learning/ch11/Ex5.scala
+++ src/main/scala/pl/softech/learning/ch11/Ex5.scala
@@ -1,8 +1,17 @@
 package pl.softech.learning.ch11
 
+import pl.softech.learning.ch11.Assertion._
+import pl.softech.learning.ch11.Monad._
+import pl.softech.learning.ch11.MonadInstances._
+
 object Ex5 {
 
   def main(args: Array[String]): Unit = {
+
+    replicateM(3, Option(1)) === Some(List(1, 1, 1))
+    replicateM(3, None) === None
+    replicateM(2, List(1, 2)) === List(List(1, 1), List(1, 2), List(2, 1), List(2, 2))
+    replicateM(3, List.empty) === List.empty
 
   }
 
