eb032fb9b42f317896274638d341f07eff1e1fd8
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-11-03 20:06:55 +0100

    ch11.Ex7

--- src/main/scala/pl/softech/learning/ch11/Ex7.scala
+++ src/main/scala/pl/softech/learning/ch11/Ex7.scala
@@ -1,9 +1,9 @@
 package pl.softech.learning.ch11
 
-import Monad._
-import MonadInstances._
-import MonadSyntax._
-import Assertion._
+import pl.softech.learning.ch11.Assertion._
+import pl.softech.learning.ch11.Monad._
+import pl.softech.learning.ch11.MonadInstances._
+import pl.softech.learning.ch11.MonadSyntax._
 
 import scala.util.Try
 
@@ -11,7 +11,7 @@ object Ex7 {
 
   trait MonadCombinators {
 
-    def compose[A,B,C, F[_] : Monad](f: A => F[B], g: B => F[C]): A => F[C] = a => for {
+    def compose[A, B, C, F[_] : Monad](f: A => F[B], g: B => F[C]): A => F[C] = a => for {
       b <- f(a)
       c <- g(b)
     } yield c
@@ -20,15 +20,23 @@ object Ex7 {
 
   def main(args: Array[String]): Unit = {
 
-    val parse : String => Option[Int] = x => Try(x.toInt).toOption
+    val parse: String => Option[Int] = x => Try(x.toInt).toOption
 
-    val inc : Int => Option[Int] = x => Some(x + 1)
+    val inc: Int => Option[Int] = x => Some(x + 1)
+
+    val toString: Int => Option[String] = x => Some(x.toString)
 
     val parseAndInc = compose(parse, inc)
 
     parseAndInc("1") === Some(2)
 
     parseAndInc("a") === None
+
+    def associativeLaw[A, B, C, D, F[_] : Monad](f: A => F[B], g: B => F[C], h: C => F[D], a: A): Unit = {
+      compose(compose(f, g), h).apply(a) === compose(f, compose(g, h)).apply(a)
+    }
+
+    associativeLaw(parse, inc, toString, "1")
 
   }
 
