58a4f576bb557fdbe1b1c48a7fac598b656baebd
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-11-03 21:25:31 +0100

    ch11.Ex8

--- src/main/scala/pl/softech/learning/ch11/Ex8.scala
+++ src/main/scala/pl/softech/learning/ch11/Ex8.scala
@@ -1,8 +1,32 @@
 package pl.softech.learning.ch11
 
+import pl.softech.learning.ch11.Assertion._
+import pl.softech.learning.ch11.Monad._
+import pl.softech.learning.ch11.MonadInstances._
+
+import scala.util.Try
+
 object Ex8 {
 
+  def flatMap[A, B, F[_] : Monad](fa: F[A])(f: A => F[B]): F[B] = {
+
+    val f0: F[A] => F[A] = _ => fa
+
+    val ff: F[A] => F[B] = compose(f0, f)
+
+    ff(fa)
+  }
+
+
   def main(args: Array[String]): Unit = {
+
+    def parse(x: String): Option[Int] = Try(x.toInt).toOption
+
+    val toList: Option[Int] => List[Int] = _.toList
+
+    flatMap(Option("1"))(parse) === Some(1)
+
+    flatMap(List("1", "2", "3"))(parse _ andThen toList) === List(1, 2, 3)
 
   }
 
