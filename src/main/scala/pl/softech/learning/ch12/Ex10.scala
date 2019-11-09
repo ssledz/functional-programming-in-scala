61c3b92340df3ac08b07b524934780dd0991d374
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-11-09 12:38:21 +0100

    skipping ch12.Ex10, solved ch12.Ex11

--- src/main/scala/pl/softech/learning/ch12/Ex10.scala
+++ src/main/scala/pl/softech/learning/ch12/Ex10.scala
@@ -3,7 +3,9 @@ package pl.softech.learning.ch12
 object Ex10 {
 
   def main(args: Array[String]): Unit = {
-    
+
+    // TODO Prove that this composite applicative functor meets the applicative laws. This is an extremely challenging exercise.
+
   }
 
 }
--- src/main/scala/pl/softech/learning/ch12/Ex11.scala
+++ src/main/scala/pl/softech/learning/ch12/Ex11.scala
@@ -1,9 +1,20 @@
 package pl.softech.learning.ch12
 
+import pl.softech.learning.ch11.Monad
+
 object Ex11 {
 
-  def main(args: Array[String]): Unit = {
-    
-  }
+  def compose[F[_], G[_]](implicit F: Monad[F], G: Monad[G]): Monad[λ[A => F[G[A]]]] = new Monad[λ[A => F[G[A]]]] {
 
+    def pure[A](a: A): F[G[A]] = F.pure(G.pure(a))
+
+    private def extract[B](gfgb: G[F[G[B]]]): F[G[B]] = ???
+
+    def flatMap[A, B](fa: F[G[A]])(f: A => F[G[B]]): F[G[B]] = F.flatMap(fa) { ga =>
+      val gfgb: G[F[G[B]]] = G.map(ga) { a =>
+        f(a)
+      }
+      extract(gfgb)
+    }
+  }
 }
