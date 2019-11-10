d8c90e1f6b30ba0c2ce3abee758a8cf2bb3b23e3
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-11-11 00:10:21 +0100

    ch12.Ex17

--- src/main/scala/pl/softech/learning/ch12/Ex17.scala
+++ src/main/scala/pl/softech/learning/ch12/Ex17.scala
@@ -1,9 +1,23 @@
 package pl.softech.learning.ch12
 
+import pl.softech.learning.Assertion._
+import pl.softech.learning.ch12.TraverseInstances._
+
 object Ex17 {
 
+  def foldLeft[A, B, F[_] : Traverse](fa: F[A])(z: B)(f: (B, A) => B): B =
+    Traverse[F].mapAccum(fa, z)((x, acc) => ((), f(acc, x)))._2
+
   def main(args: Array[String]): Unit = {
-    
+
+    val xs = List(1, 2, 3, 4, 5)
+
+    foldLeft(xs)(List.empty[Int])((ys, y) => y :: ys) === xs.reverse
+
+    foldLeft(Option(1))(0)(_ + _) === 1
+
+    foldLeft(None: Option[Int])(0)(_ + _) === 0
+
   }
 
 }
