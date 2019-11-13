4da2fefd7a0e3e95d3ff8c0ade25da5f72f6819b
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-11-13 01:17:08 +0100

    ch13.Ex1

--- src/main/scala/pl/softech/learning/ch13/Ex1.scala
+++ src/main/scala/pl/softech/learning/ch13/Ex1.scala
@@ -1,8 +1,35 @@
 package pl.softech.learning.ch13
 
+import pl.softech.learning.Assertion._
+import pl.softech.learning.ch11.Monad
+import pl.softech.learning.ch13.Free._
+import pl.softech.learning.ch7.parallelism.NonBlocking
+import pl.softech.learning.ch7.parallelism.NonBlocking.Par
+
+
 object Ex1 {
 
+  type TailRec[A] = Free[Function0, A]
+
+  type Async[A] = Free[Par, A]
+
   def main(args: Array[String]): Unit = {
+
+    val F = Monad[TailRec]
+
+    val G = Monad[Async]
+
+    F.pure(1) === Return(1)
+
+    G.pure(1) === Return(1)
+
+    def inc(x: Int): Function0[Int] = () => x + 1
+
+    def incPar(x: Int): Par[Int] = NonBlocking.unit(x + 1)
+
+    println(F.pure(1).flatMap(x => Suspend(inc(x))))
+
+    println(G.pure(1).flatMap(x => Suspend(incPar(x))))
 
   }
 
---
+++ src/main/scala/pl/softech/learning/ch13/Free.scala
@@ -0,0 +1,55 @@
+package pl.softech.learning.ch13
+
+import pl.softech.learning.ch11.Monad
+import pl.softech.learning.ch13.Free.FlatMap
+
+import scala.annotation.tailrec
+
+sealed trait Free[F[_], A] {
+
+  self =>
+
+  def flatMap[B](f: A => Free[F, B]): Free[F, B] = FlatMap(self, f)
+
+  def map[B](f: A => B): Free[F, B] = flatMap(a => Free.pure(f(a)))
+
+}
+
+object Free {
+
+  def pure[F[_], A](a: A): Free[F, A] = Return(a)
+
+  def suspend[F[_], A](fa: => F[A]): Free[F, A] = FlatMap(pure(()), (_: Unit) => Suspend(fa))
+
+  @tailrec
+  private def step[F[_], A](free: Free[F, A]): Free[F, A] = free match {
+    case FlatMap(FlatMap(x, f), g) => step(x flatMap (a => f(a) flatMap g))
+    case FlatMap(Return(x), f) => step(f(x))
+    case _ => free
+  }
+
+  def run[F[_], A](free: Free[F, A]): F[A] = step(free) match {
+    case Return(a) => ???
+    case Suspend(r) => r
+    case FlatMap(x, f) => x match {
+      case Suspend(r) => ???
+      case _ => sys.error("Impossible; `step` eliminates these cases")
+    }
+  }
+
+  implicit def freeMonadInstance[F[_]]: Monad[Lambda[A => Free[F, A]]] = new Monad[Lambda[A => Free[F, A]]] {
+
+    override def flatMap[A, B](fa: Free[F, A])(f: A => Free[F, B]): Free[F, B] = fa.flatMap(f)
+
+    override def pure[A](a: A): Free[F, A] = Free.pure(a)
+  }
+
+  case class Return[F[_], A](a: A) extends Free[F, A]
+
+  case class Suspend[F[_], A](fa: F[A]) extends Free[F, A]
+
+  case class FlatMap[F[_], A, B](fa: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
+
+}
+
+
